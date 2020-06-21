e20fb6e2f467358ef625029c3dc23f1c8f633c0a
dangspin <dangxiaoqian@gmail.com>
2020-06-21 10:20:40 +0800

    对webserver的修改

--- src/main.js
+++ src/main.js
@@ -1,7 +1,42 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    response.write('hello~~~');
+    // console.log(request['headers']['user-agent']);
+
+    // response.writeHead(200, {
+    //     'Content-Type': 'text/html',
+    // });
+    // response.write('<input placeholder="shuru"/>');
+    // response.end();
+
+    // switch (request.url) {
+    //     case '/':
+    //         response.write("hello ~~");
+    //         break;
+    //     case '/posts':
+    //         response.write('posts');
+    //         break;
+    //     case '/signup':
+    //         response.write('signup');
+    //         break;
+    //     default:
+    //         response.writeHead(404);
+    //         response.write('404');
+    //         break;
+    // }
+    const data = {
+        id: 1,
+        title: '关山月',
+        content: '明月出天山，苍茫云海间'
+    };
+
+    const to_json = JSON.stringify(data);
+
+    response.writeHead(200, {
+        'Content-Type': 'application/json; charset=utf-8',
+    });
+
+    response.write(to_json);
     response.end();
 });
 
